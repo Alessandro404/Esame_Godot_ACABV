a257dff5f2f3928e4e0e36a1e0fde0fda1be3fd1
Alessandro404 <a.chachlakis@gmail.com>
2024-12-31 17:17:57 +0100

    rimosso phantomcamera

--- Personaggi/player.gd
+++ Personaggi/player.gd
@@ -1,28 +1,14 @@
 extends CharacterBody3D
 
+@export_group("Camera")
+@export_range(0.0, 1.0) var mouse_sensitivity := 0.25
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+var _camera_input_direction := Vector2.ZERO
 
+func _unhandled_input(event:InputEvent) -> void:
+	var iscamera_motion := (
+		event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED
+	)
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("move_left", "move_right", "move_foward", "move_backward")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
-	move_and_slide()
+if is_camera_motion:
+	_camera_input_direction = event.screen_relative * mouse_sensitivity
