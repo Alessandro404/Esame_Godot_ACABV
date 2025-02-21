57508fd3f4ad2e10c9865cab1f177f33e56565ec
Alessandro404 <a.chachlakis@gmail.com>
2025-02-21 11:54:08 +0100

    aggiunta grafica portale

--- scenes/portal.gd
+++ scenes/portal.gd
@@ -6,6 +6,12 @@ extends Node3D
 
 @export var spawn_point:Node3D
 
+@export var tint_color: Color
+@onready var portal_sprites = $Area3D/AnimatedSprite3D
 
 func _on_area_3d_body_entered(_body):
 	get_tree().current_scene.teleport(destination_room_id,destination_portal_id)
+
+
+func _ready():
+	portal_sprites.modulate = tint_color
