2f30b6ef184c3a687d701cebffadcf394b6a9e0d
Alessandro404 <a.chachlakis@gmail.com>
2025-02-23 20:31:36 +0100

    fatte robe ai portali e alle stanze

---
+++ scenes/flickeringLight.gd
@@ -0,0 +1,20 @@
+extends OmniLight3D
+
+var time_passed = 0.0
+var texture 
+
+func _ready():
+	texture = NoiseTexture2D.new()
+	texture.bump_strength = 0.7
+	texture.noise = FastNoiseLite.new()
+	texture.noise.set_seed( randi())
+	texture.noise.frequency = 0.05
+	await texture.changed
+
+func _process(delta):
+	time_passed += delta
+
+	var sampled_noise = texture.noise.get_noise_1d(time_passed)
+	sampled_noise = abs(sampled_noise)
+	light_energy = sampled_noise*200+1
+	print(light_energy)
--- scenes/portal.gd
+++ scenes/portal.gd
@@ -5,6 +5,7 @@ extends Node3D
 @export var destination_portal_id:int
 
 @export var spawn_point:Node3D
+@onready var light = $OmniLight3D
 
 @export var tint_color: Color
 @onready var portal_sprites = $Area3D/AnimatedSprite3D
@@ -15,3 +16,4 @@ func _on_area_3d_body_entered(_body):
 
 func _ready():
 	portal_sprites.modulate = tint_color
+	light.set_color(tint_color)
